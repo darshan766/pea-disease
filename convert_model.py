116914b844f05284d8414ce5850108bf187a9a9a
Darshan P <darshanhareesh@gmail.com>
2026-03-14 01:22:43 +0530

    fix: switch to TFLite model to reduce memory usage

--- convert_model.py
+++ convert_model.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
+import os
 
-model = tf.keras.models.load_model("pea_disease_model.keras")
+print("Loading model...")
+model = tf.keras.models.load_model("pea_disease_model.keras", compile=False)
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
+print("Saving as SavedModel format...")
+model.export("saved_model_temp")
+
+print("Converting to TFLite...")
+converter = tf.lite.TFLiteConverter.from_saved_model("saved_model_temp")
 tflite_model = converter.convert()
 
 with open("pea_disease_model.tflite", "wb") as f:
     f.write(tflite_model)
 
-print("Model converted to TFLite successfully.")
-print(f"TFLite model size: {len(tflite_model) / 1024 / 1024:.2f} MB")
+# Cleanup temp folder
+import shutil
+shutil.rmtree("saved_model_temp")
+
+print(f"Done! TFLite model size: {len(tflite_model) / 1024 / 1024:.2f} MB")
